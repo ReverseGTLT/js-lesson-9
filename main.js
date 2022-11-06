5f44532180d695f1a81375f598aa70a3ac2e5186
ReverseGTLT <108753232+ReverseGTLT@users.noreply.github.com>
2022-11-06 20:17:28 +0200

    upd3: added join

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ while (!arrayNew.includes(null) && !arrayNew.some(isNaN)) {
     arrayNew.push(prompt('Enter numbers'))
 }
 arrayNew.pop();
-console.log(`The state of the array is as follows: ${arrayNew}`)
+console.log(`The state of the array is as follows: ${arrayNew.join(', ')}`)
 arrayNew.sort(function (a, b) {
     return a - b;
 });
-console.log(`The state of the array after sort: ${arrayNew}`)
+console.log(`The state of the array after sort: ${arrayNew.join(', ')}`)
 arrayNew.splice(1,3);
-console.log(`The state of the array after splice: ${arrayNew}`)
+console.log(`The state of the array after splice: ${arrayNew.join(', ')}`)
